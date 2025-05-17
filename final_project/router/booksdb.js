3e8e35d4c5f81ee7186da8f2da3f64da20e190e8
Hossam-97 <hossam_1997@live.com>
2025-05-17 12:57:42 -0400

    finished first 4 tasks

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,3 +1,5 @@
+const { Router } = require("express");
+
 let books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,42 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  new Promise ((resolve,reject)=>{
+const book = books[isbn];
+if (book) resolve(book);
+else reject("Book not found");
+})
+.then((book)=> res.status(200).json(book))
+.catch((err) => res.status(404).json({message:err}))
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  new Promise((resolve) => {
+    const filteredBooks = Object.values(books).filter(book => book.author === author);
+    resolve(filteredBooks);
+  }).then(result => {
+    res.status(200).json(result.length ? result : { message: "No books by that author" });
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+  new Promise((resolve) => {
+    const filteredBooks = Object.values(books).filter(book => book.title === title);
+    resolve(filteredBooks);
+  }).then(result => {
+    res.status(200).json(result.length ? result : { message: "No books by that author" });
+  });
 });
 
 //  Get book review
